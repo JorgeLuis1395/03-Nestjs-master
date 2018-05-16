80b0e5145f352d762e700f7692d00233a2bae9aa
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-05-16 12:45:56 -0500

    Initial commit

---
+++ Ejemplo/src/app.module.ts
@@ -0,0 +1,40 @@
+import {MiddlewaresConsumer, Module, NestModule} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {UsuarioController} from "./usuario.controller";
+import {ParametrosController} from "./parametros.controller";
+import {LogMiddleware} from "./log.middleware";
+import {UsuarioService} from "./usuario.service";
+
+
+@Module({
+    imports: [ // OtrosModulos
+
+    ],
+    providers: [
+        UsuarioService
+    ],
+    controllers: [ // Controladores
+        AppController,
+        UsuarioController,
+        ParametrosController
+    ],
+    components: [],
+})
+export class AppModule implements NestModule {
+    nombreAplicacion = 'EPN';
+
+    configure(consumer: MiddlewaresConsumer)
+        : void {
+        consumer
+            .apply(LogMiddleware)
+            .with(this.nombreAplicacion, 1989)
+            .forRoutes(
+                UsuarioController,
+                AppController,
+                ParametrosController
+            )
+        //.apply(OtroMiddleware)
+        //.forRoutes(Otras rutas);
+    }
+
+}
